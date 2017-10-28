06042058f9e25ad1078d48e5f9b076bf11be6bd7
milerza <camilasilalves@outlook.com>
2017-10-28 08:43:22 -0200

    trabalho enviado

---
+++ camila-questao2.js
@@ -0,0 +1,150 @@
+db.localizacao.insert({
+	name:"Luana Ramos",
+	location:{type:"Point", coordinates:[-43.3160809,-15.8052074]}
+})
+
+db.localizacao.insert({
+	name:"Joana D'arc",
+	location:{type:"Point", coordinates:[-43.310403,-15.7943479]}
+})
+
+db.localizacao.insert({
+	name:"Alberto Souza Cunha",
+	location:{type:"Point", coordinates:[-43.3179225,-15.8057438]}
+})
+
+db.localizacao.insert({
+	name:"Layla Teodora",
+	location:{type:"Point", coordinates:[-43.3179687,-15.8198619]}
+})
+
+db.localizacao.insert({
+	name:"Thalia Santos",
+	location:{type:"Point", coordinates:[-43.341919,-15.792172]}
+})
+
+db.localizacao.insert({
+	name:"Emilly Raissa",
+	location:{type:"Point", coordinates:[-43.312611,-15.807591]}
+})
+
+db.localizacao.insert({
+	name:"Sávia Fernandes Marques",
+	location:{type:"Point", coordinates:[-43.3223997,-15.7814005]}
+})
+
+db.localizacao.insert({
+	name:"Dieyse Michele Leal",
+	location:{type:"Point", coordinates:[-43.323578,-15.781380]}
+})
+
+db.localizacao.insert({
+	name:"Vitória Ingred de Oliveira",
+	location:{type:"Point", coordinates:[-43.314893,-15.817489]}
+})
+
+db.localizacao.insert({
+	name:"João Vitor Silva Matos",
+	location:{type:"Point", coordinates:[-43.306551,-15.8147075]}
+})
+
+db.localizacao.insert({
+	name:"Michelle de Oliveira",
+	location:{type:"Point", coordinates:[-43.8263155,-15.31233914]}
+})
+
+db.localizacao.insert({
+	name:"Maria Thaís Rodrigues",
+	location:{type:"Point", coordinates:[-43.0288791,-15.7377377]}
+})
+
+db.localizacao.insert({
+	name:"Ádrian Marcello Mendes Dias",
+	location:{type:"Point", coordinates:[-43.304557,-15.811190]}
+})
+
+db.localizacao.insert({
+	name:"Lara Mariana Gomes Pereira",
+	location:{type:"Point", coordinates:[-43.3153666,-15.8197566]}
+})
+
+db.localizacao.insert({
+	name:"Camila Silva Alves",
+	location:{type:"Point", coordinates:[-43.2960149,-15.8251198]}
+})
+
+db.localizacao.insert({
+	name:"Lívia Samara Cardoso",
+	location:{type:"Point", coordinates:[-43.314748,-15.7953561]}
+})
+
+db.localizacao.insert({
+	name:"Micaelle Alves Martins",
+	location:{type:point, coordinates:[-43.3148092,-15.8221044]}
+})
+
+db.localizacao.insert({
+	name:"Flora Mendes Pinheiro Caires",
+	location:{type:"Point", coordinates:[-43.3051085,-15.8018395]}
+})
+
+db.localizacao.insert({
+	name:"Jade Passos Almeida",
+	location:{type:"Point", coordinates:[-43.309728,-15.819087]}
+})
+
+db.localizacao.insert({
+	name:"Sidney Emanuel Soares",
+	location:{type:"Point", coordinates:[-43.3121088,-15.80360014]}
+})
+
+db.localizacao.insert({
+	name:"Lavínia Tamires Souza Araújo",
+	location:{type:"Point", coordinates:[-43.3168428,-15.8045297]}
+})
+
+db.localizacao.insert({
+	name:"Victória Felipe Damascena Souza",
+	location:{type:"Point", coordinates:[-43.300751,-15.818761]}
+})
+
+db.localizacao.insert({
+	name:"Emille Beatriz",
+	location:{type:"Point", coordinates:[-43.3125666,-15.8039344]}
+})
+
+db.localizacao.insert({
+	name:"Hellen Aline R Silva",
+	location:{type:"Point", coordinates:[-43.3165794,-15.7990549]}
+})
+
+db.localizacao.insert({
+	name:"Joyce Marianne",
+	location:{type:"Point", coordinates:[-43.3087511,-15.8290764]}
+})
+
+db.localizacao.insert({
+	name:"Amanda Pereira Daniel",
+	location:{type:"Point", coordinates:[-43.314899,-15.802350]}
+})
+
+db.localizacao.insert({
+	name:"Luan Alves Vieira",
+	location:{type:"Point", coordinates:[-43.3138138,-15.8007558]}
+})
+
+db.localizacao.insert({
+	name:"João Gabriel de A silva",
+	location:{type:"Point", coordinates:[-43.8049733,-15.8489078]}
+})
+
+db.localizacao.insert({
+	name:"Daniel Barbosa Mendes",
+	location:{type:"Point", coordinates:[-43.43084923,-15.8025955]}
+})
+
+db.localizacao.insert({
+	name:"Tom Roberto Ruas Costa",
+	location:{type:"Point", coordinates:[-43.3090354,-15.802596]}
+})
+
